3df69c84f5cff9ddc73b66eadbec54a8d9e4b32b
Jefferson <jeffersongarcia1599@gmail.com>
2021-09-26 05:35:26 -0500

    I FORGOT AN S IN THE ROUTERS PACKAGE!!!!!!

--- db/checkIfUserExists.go
+++ db/checkIfUserExists.go
@@ -8,10 +8,22 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func CheckIfUserExists(email string) (models.User, bool, error) {
+func CheckIfUserExists(email string) (models.User, bool, string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	db := MongoCN.Database("twitter")
-	col
+	col := db.Collection("users")
+
+	condition := bson.M{"email": email}
+
+	var result models.User
+
+	err := col.FindOne(ctx, condition).Decode(&result)
+	ID := result.ID.Hex()
+
+	if err != nil {
+		return result, false, ID
+	}
+	return result, true, ID
 }
--- routers/signup.go
+++ routers/signup.go
@@ -1,4 +1,4 @@
-package router
+package routers
 
 import (
 	"encoding/json"
