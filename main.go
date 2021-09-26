780c3c68d47b2adcc07a0bc0b9cc6deec45bdb60
Jefferson <jeffersongarcia1599@gmail.com>
2021-09-26 00:49:37 -0500

    Working on the db connection and handlers

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"github.com/JeffersonGarcia15/db"
+	"github.com/JeffersonGarcia15/handlers"
+)
+
 func main() {
-	
+	if db.ConnectDB() == 0 {
+		log.Fatal("No connection to database")
+		return
+	}
+	handlers.Urls()
 }
