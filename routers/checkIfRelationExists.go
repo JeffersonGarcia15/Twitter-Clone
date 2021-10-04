f0a1afc6d2696cccaed1f3b6ef7e0a2700a93646
Jefferson <jeffersongarcia1599@gmail.com>
2021-10-04 07:38:48 -0500

    Working on suggested users feature on routers

---
+++ routers/checkIfRelationExists.go
@@ -0,0 +1,33 @@
+package routers	
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/JeffersonGarcia15/Twitter-Clone/db"
+	"github.com/JeffersonGarcia15/Twitter-Clone/models"
+
+)
+
+
+func CheckIfRelationExists(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "You must send the id parameter", http.StatusBadRequest)
+		return
+	}
+
+	var t models.Relation
+	t.UserID = IDUser
+	t.UserRelationID = ID
+
+	var response models.CheckIfRelationExists
+
+	status, err := db.CheckIfRelationExists(t)
+	if err != nil || !status {
+		response.Status = true
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(response)
+}
