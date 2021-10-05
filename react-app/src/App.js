079e889c093aa08fba160d4eabb30d59d2ca7608
Jefferson <jeffersongarcia1599@gmail.com>
2021-10-05 09:52:07 -0500

    Cleaned up code and installed react-bootstrap

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.scss';
+import { useState } from 'react'
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? <h1>You are logged in</h1> : <h1>You are not logged in</h1>}
+      
     </div>
-  );
+  )
 }
 
-export default App;
