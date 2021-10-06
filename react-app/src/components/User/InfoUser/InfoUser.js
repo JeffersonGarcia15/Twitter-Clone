23c8e7a3db7821fc82e3cbe50d49333f1f2f5b55
Jefferson <jeffersongarcia1599@gmail.com>
2021-10-06 14:26:05 -0500

    Formatted Date by using momentjs

--- react-app/src/components/User/InfoUser/InfoUser.js
+++ react-app/src/components/User/InfoUser/InfoUser.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Location, Link, BirthDate } from '../../../utils/icons'
-
+import moment from 'moment'
+// import localization from "moment/locale/en"
 import "./InfoUser.scss"
 
 export default function InfoUser(props) {
@@ -18,10 +19,21 @@ export default function InfoUser(props) {
                     {user.biography}
                 </div>
             )}
-            <div>
+            <div className='more-info'>
                 {user?.location && (
                     <p>
+                        <Location />
                         {user.location}
+                    </p>
+                )}
+                {user?.website && (
+                    <a href={user.website} alt={user.website} target="_blank" rel="noopener noreferrer"><Link /> {user.website}</a>
+                )}
+                {user?.bod && (
+                    <p>
+                        <BirthDate />
+                        {moment(user.bod).format("LL")}
+
                     </p>
                 )}
             </div>
