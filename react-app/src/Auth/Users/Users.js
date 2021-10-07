d13b532febc614b25d0047e8a7115814f3295a5c
Jefferson <jeffersongarcia1599@gmail.com>
2021-10-07 16:19:39 -0500

    Working on params with queryString to make it dynamic

--- react-app/src/Auth/Users/Users.js
+++ react-app/src/Auth/Users/Users.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, useLocation } from 'react-router-dom'
 import BasicLayout from '../../layout/BasicLayout'
+import queryString from 'query-string'
 import { Spinner, ButtonGroup, Button } from "react-bootstrap";
+import { getFollowsApi } from '../../api/follow'
 
 import "./Users.scss"
 
 export default function Users(props) {
     const { setRefreshCheckLogin } = props
+
+    const [users, setUsers] = useState(null)
+    const location = useLocation()
+    const params = useUsersQuery(location)
+    console.log(params)
+    console.log('dddddd', queryString.stringify(params))
+
+    useEffect(() => {
+        getFollowsApi(queryString.stringify(params)).then(response => {
+            console.log(response)
+        }).catch(() => {
+            setUsers([])
+        })
+    })
     return (
         <BasicLayout className="users" title="Users" setRefreshCheckLogin={setRefreshCheckLogin}>
             <div className="users__title">
@@ -22,4 +39,10 @@ export default function Users(props) {
             </ButtonGroup>
         </BasicLayout>
     )
+}
+
+function useUsersQuery(location) {
+    const { page = 1, searchType = 'follow', search = ''} = queryString.parse(location.search)
+
+    return {page, searchType, search}
 }
