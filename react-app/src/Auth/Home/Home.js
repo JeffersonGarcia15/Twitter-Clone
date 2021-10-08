0156ea05c7ab246cedd0a64b327b51d4246ee9c3
Jefferson <jeffersongarcia1599@gmail.com>
2021-10-08 08:43:19 -0500

    Basic skeleton for home page

--- react-app/src/Auth/Home/Home.js
+++ react-app/src/Auth/Home/Home.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BasicLayout from "../../layout/BasicLayout"
+import { getTweetsFollowersApi } from '../../api/tweet'
 
 import "./Home.scss"
 
 export default function Home(props) {
+    const [tweets, setTweets] = useState(null)
+    const [page, setPage] = useState(1)
+
+    useEffect(() => {
+        getTweetsFollowersApi(page).then(response => {
+            console.log(response)
+        })
+    }, [page])
     const { setRefreshCheckLogin } = props
     return (
         <BasicLayout className='home' setRefreshCheckLogin={setRefreshCheckLogin}>
-                    <h2>Hello from the home page</h2>
-
-                </BasicLayout>
+            <div className="home__title">
+                <h2>Tweets</h2>
+            </div>
+            <p>Tweet List</p>
+            <p>More Tweets</p>
+        </BasicLayout>
     )
 }
